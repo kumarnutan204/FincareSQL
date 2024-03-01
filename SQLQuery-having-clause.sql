23b273fc9a63fab231095235695522af44a52388
kumarnutan204 <kumarnutan204@gmail.com>
2024-03-01 17:28:54 +0530

    [Nutan] Added: implemented having clause with group by

---
+++ SQLQuery-having-clause.sql
@@ -0,0 +1,9 @@
+select * from employee_info;
+
+select deptID, SUM(empSalary) as 'Total Salary' from employee_info group by deptID having deptID=9;
+
+select deptID, SUM(empSalary) as 'Total Salary' from employee_info where deptID=9 group by deptID;
+
+--both of the above will give the same result 
+--you either use the where clasuse first and then group by or having clause right after group by clause
+
