be2b9f384bfa51ac0753c2c20ccfb4590b5485df
kumarnutan204 <kumarnutan204@gmail.com>
2024-03-01 17:34:30 +0530

    [Nutan] Added: implemented having clause

--- SQLQuery-having-clause.sql
+++ SQLQuery-having-clause.sql
@@ -4,6 +4,9 @@ select deptID, SUM(empSalary) as 'Total Salary' from employee_info group by dept
 
 select deptID, SUM(empSalary) as 'Total Salary' from employee_info where deptID=9 group by deptID;
 
+
 --both of the above will give the same result 
 --you either use the where clasuse first and then group by or having clause right after group by clause
 
+
+select deptID, SUM(empSalary) as 'Total Salary' from employee_info group by deptID having SUM(empSalary) BETWEEN 80000 AND 100000 ;
