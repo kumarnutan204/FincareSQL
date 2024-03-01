a6647af077bfe4342e874ba02ac315da1079e4b9
kumarnutan204 <kumarnutan204@gmail.com>
2024-03-01 16:06:51 +0530

    [Nutan] Added: numerical functions like sin cos abs implemented

---
+++ SQLQuery-numerical-funct.sql
@@ -0,0 +1,15 @@
+--Numerical functions
+
+select abs(-10);
+
+select CEILING(43.27);
+
+select FLOOR(638.9762);
+
+select SIGN(-25), SIGN(0), SIGN(45);
+
+select SQUARE(6), SQRT(81), PI(), SIN(30),COS(30),tan(30);
+
+select ASIN(0.5);
+
+select EXP(2);
