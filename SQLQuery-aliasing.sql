be948a7308ab688f8dcd7d81f2d6aa58aa85b5c7
kumarnutan204 <kumarnutan204@gmail.com>
2024-03-02 16:14:45 +0530

    [Nutan] Added: aliasing of table and column implemented

---
+++ SQLQuery-aliasing.sql
@@ -0,0 +1,7 @@
+
+SELECT * FROM employee_info;
+
+SELECT empSalary FROM employee_info;
+
+SELECT empSalary AS 'Employee Salary' FROM employee_info;
+SELECT empSalary 'Employee Salary' FROM employee_info;
