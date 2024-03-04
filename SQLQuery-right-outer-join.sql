0e88731b044c24168c93b9e0ccf0246d6e893c28
kumarnutan204 <kumarnutan204@gmail.com>
2024-03-04 15:24:04 +0530

    [Nutan] Added: implemented right join

---
+++ SQLQuery-right-outer-join.sql
@@ -0,0 +1,12 @@
+
+select * from employee_info;
+
+select * from department;
+
+
+select * from employee_info inner join department on employee_info.deptID=department.deptId;
+
+
+select * from department left outer join employee_info on employee_info.deptID=department.deptId;
+
+select * from department right outer join employee_info on employee_info.deptID=department.deptId;
