847368f10bff2bea50d72e75ec128afe5d645923
kumarnutan204 <kumarnutan204@gmail.com>
2024-03-05 12:36:46 +0530

    [Nutan] Edited: the if condition problem fixed

--- SQLQuery-while-loop.sql
+++ SQLQuery-while-loop.sql
@@ -1,10 +1,17 @@
+select * from employee_info;
+
 BEGIN
-	WHILE (SELECT MIN(empSalary) FROM employee_info ) < 80000
+	WHILE (SELECT MIN(empSalary) FROM employee_info ) < 150000
 	BEGIN
 		UPDATE employee_info SET empSalary = empSalary + 10000; PRINT 'Salary updated';
 		select * from employee_info;
-		IF (SELECT MIN(empSalary) FROM employee_info) >= 80000
-			PRINT 'All salaries updated to >= 80000.';
+		IF (SELECT MIN(empSalary) FROM employee_info) >= 150000
+			begin
+			PRINT 'All salaries updated to >= 150000.';
 			BREAK;
+			end
 	END
 END
+
+select * from employee_info;
+go
